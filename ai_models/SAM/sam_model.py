07b3080cc4d40508236063ef2e6073ab90e855a3
aditya-5842 <singhadityastudy@gmail.com>
2023-10-29 18:42:51 +0530

    gpu/cpu support

--- ai_models/SAM/sam_model.py
+++ ai_models/SAM/sam_model.py
@@ -6,8 +6,8 @@ import supervision as sv
 
 class SAM:
     def __init__(self, MODEL_TYPE="vit_b", MODEL_PATH="sam_vit_b_01ec64.pth"):
-        self.DEVICE = torch.device("cpu")
-        # self.DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        # self.DEVICE = torch.device("cpu")
+        self.DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.MODEL_TYPE = MODEL_TYPE
         self.CHECKPOINT_PATH = os.path.join(os.getcwd(), "data/weights", MODEL_PATH)
         self.sam = sam_model_registry[MODEL_TYPE](checkpoint=self.CHECKPOINT_PATH).to(device=self.DEVICE)
